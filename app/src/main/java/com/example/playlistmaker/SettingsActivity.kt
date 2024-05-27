adf4b751ccbb81ac2558f241fc3c85bf52754e9c
Andrei Semenov <sand-nlp@ya.ru>
2024-05-27 21:55:57 +0300

    Интенты на экране настроек

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -20,10 +23,24 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
-
         setSupportActionBar(toolbar)
         supportActionBar?.title = getString(R.string.settings_title)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val shareText = findViewById<TextView>(R.id.share_text)
+        shareText.setOnClickListener {
+            shareApp()
+        }
+
+        val mailToSupport = findViewById<TextView>(R.id.support_send)
+        mailToSupport.setOnClickListener {
+            sendSupportEmail()
+        }
+
+        val userAgreement = findViewById<TextView>(R.id.user_lic)
+        userAgreement.setOnClickListener {
+            openUserAgreement()
+        }
 
     }
 
@@ -35,5 +52,35 @@ class SettingsActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun shareApp(){
+        val shareIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.android_dev_url))
+        }
+        startActivity(Intent.createChooser(shareIntent, getString(R.string.share_header)))
+    }
+
+    private fun sendSupportEmail(){
+        val mailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.user_email)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_body))
+        }
+
+        if (mailIntent.resolveActivity(packageManager) != null)
+            startActivity(mailIntent)
+    }
+
+    private fun openUserAgreement(){
+        val urlIntent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(getString(R.string.user_agreement))
+        }
+
+        if (urlIntent.resolveActivity(packageManager) != null)
+            startActivity(urlIntent)
     }
 }
